37a88871959b2da97a5a05b6c49158bf5555978d
gibtmirdas <gibtmirdas@gmail.com>
2013-12-04 17:53:29 +0100

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BacchusTmp::Application.config.secret_key_base = '3127ab09931ee9ec868f6b484c43650d1a677c2599e4d91ff42fbc0e2b9d194e5674081a2fe52235d35a9e0a4a85644277baec06f09b287051564ee52b9bcd27'
