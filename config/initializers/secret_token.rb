a175d48cb6aacc470bc3bdcbe9f4d395c6867daa
Hadrien Froger <hadrien.froger@realproject.ch>
2014-02-09 19:34:16 +0100

    try Slim, and it's quiet big.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bacchus::Application.config.secret_key_base = '79180953bbcc051b1df2fdac6e768035c1de5f81008a4fedf24b0e450c8fe00c16393f8203b2305f8ba1a27bb012ad711dea528fe089bf46bc19b214fbf3cfcf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bacchus::Application.config.session_store :cookie_store, key: '_bacchus_session'
