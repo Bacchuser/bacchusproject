7236b6bed7ea2fe2afbebe0659f8b0ccbca7776b
Hadrien Froger <hadrien.froger@realproject.ch>
2014-03-23 23:11:05 +0100

    add some comments, remove dead code

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,28 @@
+
+# +---------------------------------+
+# | Event instance                  |      An instance of event task is
+# |---------------------------------|      particular : It has a reference
+# |                                 |      on the task node. And the task node
+# |                                 |      has a reference on the event. All the
+# | +---------------------------+   |      task node of the childs will have this
+# | | Task (Node in the tree)  <--------+  link. Like this, we can query only
+# | |---------------------------|   |   |  nodes in the event_id given.
+# | |Label : String             |   |   |
+# | |Tree Stuff  ...            |   |   |
+# | |event_id : Integer +-------------+ |
+# | +---------------------------+   | | |
+# |                                 | | |
+# |                                 | | |
+# | +---------------------------+   | | |
+# | | Event - General infos    <------+ |
+# | |---------------------------|   |   |
+# | |Desctiption : String       |   |   |
+# | |Start_at : Date            |   |   |
+# | |End_at : Date              |   |   |
+# | |task_id : Integer +----------------+
+# | +---------------------------+   |
+# |                                 |
+# +---------------------------------+
 class Event < ActiveRecord::Base
   # Presenter from http://blog.jayfields.com/2007/03/rails-presenter-pattern.html
   include Authority::Abilities
--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,49 @@
 Bacchus::Application.routes.draw do
   devise_for :cake_plan_users
 
+  # The main controller is home. It's allowd to do two things :
+  # 1. Display all the event in list (main page)
+  # 2. Create a new event (so form and saving behaviour)
   root 'home#main'
+  resources :home do
+    collection do
+      get :new_event # Form
+      post :create_event # Saving
+    end
+  end
 
-  # The tasks ressources have all the main routes for managing a task
-  # All the logic are then done in the associate presenters, in order
-  # to keep skinny controller.
+  # All the tasks are managed by the task controller.
+  # like this, we can create/edit/display all the different
+  # task and their particularities.
+  # Because at the end, for a controller, it's always the same actions :
+  # * Save a task
+  # * Display a task
+  # * Update a task
+  # * Delete a task.
+  #
+  # So we decide to implement a reflexive method. Like this, all the
+  # task now if they have subclass or not, and which one it is.
+  # Like this, the controller display the task related with the
+  # subclass name (partial _subclassname.hml.slim).
+  # And the save/delete/update are done by dynamic linking
+  # (we start saving for the subclass, and then the subclass
+  # as the contract to go up to share the saving message).
+  #
+  # TODO
+  # [ ] the update_subtask is EVIL
+  # [ ] the new_task should be renamed in create_task
+  # [ ] delete a subtask is not done
   resources :tasks,
     :except => [:show, :new, :edit, :create, :update, :destroy],
     :constraints => { :id => /\d.*/ } do
 
     member do
+      # We display the event, with eventually a subtask to display.
       get 'admin_event(/:task_id)', :action => :admin_event, :as => :admin_event
+      # we update the subtask. Could be save/create/update.
       post 'update_subtask/:task_id', :action => :update_subtask, :as => :update_subtask
+      # create a new and empty task, with NO subclass.
       get :new_task
-    end
-  end
-
-  # No use for now. Maybe useless, was to complete the DB schema
-  resources :users
-
-  # Main page, to display login/register, and the main dashboard.
-  resources :home do
-    collection do
-      get :new_event
-      post :create_event
     end
   end
 end
