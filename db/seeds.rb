a29befea9f1620599464180c7bd282c875e3df23
Hadrien Froger <hadrien.froger@realproject.ch>
2014-09-25 16:31:32 +0200

    refactorisation des vues. Utilisations +/- massives des yield-content_for. Plus d'info ici : http://guides.rubyonrails.org/layouts_and_rendering.html#understanding-yield

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,12 +2,15 @@ puts "CREATE DEFAULTS"
 def new_users
   user = CakePlanUser.create! email: 'hadrien.froger@gmail.com',
                        password: 'bacchus123456789',
-                       password_confirmation: 'bacchus123456789'
+                       password_confirmation: 'bacchus123456789',
+                       confirmed_at: '2014-09-25 00:00:00'
+
   puts ">> user :" << user.email << ", pass : bacchus123456789"
 
   user = CakePlanUser.create! email: 'hadrien@bacchusproject.ch',
                        password: 'bacchus123456789',
-                       password_confirmation: 'bacchus123456789'
+                       password_confirmation: 'bacchus123456789',
+                       confirmed_at: '2014-09-25 00:00:00'
   puts ">> user :" << user.email << ", pass : bacchus123456789"
 
 
--- test/models/cake_plan_user_test.rb
+++ test/models/cake_plan_user_test.rb
@@ -42,7 +42,7 @@ class CakePlanUserTest < ActiveSupport::TestCase
   test "Connect user normal_user" do
     CakePlanUser.stubs(:authenticate).returns false
     user = cake_plan_users(:normal_user)
-    post :sign_in, :user => {:user_name => 'foo', :password =>'bar'}
+    post :sign_in, :user => { :user_name => 'foo', :password =>'bar'}
     assert_equals flash[:error] , "Authentication failed"
   end
 
