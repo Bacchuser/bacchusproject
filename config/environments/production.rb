716de370ac44b4023f6891e7cef566237137cf70
gibtmirdas <t.bertrand@outlook.com>
2014-10-08 22:17:50 +0200

    add mailer host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ Bacchus::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  
+  # Set mailer host
+  config.action_mailer.default_url_options = { :host => 'bacchusproject.ch' }
+
 end
